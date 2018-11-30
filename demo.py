b692d44ecc260ff85bcb808aea79e4cbe84a53f4
Liang Mi <icemiliang@gmail.com>
2018-11-29 21:01:21 -0700

    adding curvature; cleanup

--- demo.py
+++ demo.py
@@ -1,25 +1,23 @@
 # Variational Optimal Transportation
 #
 # Author: Liang Mi <liangmi@asu.edu>
-#
-# Date: Nov 28th 2018
-#
+# Date: Nov 29th 2018
 # License: MIT License
 
 from vot import *
 import matplotlib.pyplot as plt
 
-# ----- set up ------ #
+# ------- set up -------- #
 vot = Vot()
 vot.import_data_file('p.csv','e.csv')
 vot.setup(maxIterP=1, maxIterH=1500)
 
-# ----- plot before ------ #
+# ----- plot before ----- #
 plt.subplot(121)
 plt.scatter(vot.e_coor[:,0], vot.e_coor[:,1], marker='.')
 plt.scatter(vot.p_coor[:,0], vot.p_coor[:,1], marker='o')
 
-# ----- main program ------ #
+# ----- main program ---- #
 vot.cluster()
 
 # ----- plot after ------ #
@@ -27,6 +25,7 @@ plt.subplot(122)
 plt.scatter(vot.e_coor[:,0], vot.e_coor[:,1], marker='.')
 plt.scatter(vot.p_coor[:,0], vot.p_coor[:,1], marker='o')
 
+# -------- plot --------- #
 plt.xlim(-1,1)
 plt.ylim(-1,1)
 plt.show()
--- vot.py
+++ vot.py
@@ -1,9 +1,7 @@
 # Variational Optimal Transportation
 #
 # Author: Liang Mi <liangmi@asu.edu>
-#
-# Date: Nov 28th 2018
-#
+# Date: Nov 29th 2018
 # License: MIT License
 
 import numpy as np
@@ -30,14 +28,14 @@ class Vot:
     def import_data(self, dataP, dataE, mass=False):
         self.p_label = dataP[:,0]
         self.numP = self.p_label.size
-        self.p_coor = dataP[:,1:] if not mass else  dataP[:,2:]
-        self.p_dirac = np.ones(self.numP)/self.numP if not mass else dataP[:,1]
+        self.p_coor = dataP[:,2:] if mass else dataP[:,1:]
+        self.p_dirac = dataP[:,1] if mass else np.ones(self.numP)/self.numP
         self.p_mass = np.zeros(self.numP)
 
-        self.e_label = dataE[:, 0]
+        self.e_label = dataE[:,0]
         self.numE = self.e_label.size
-        self.e_coor = dataE[:, 1:] if not mass else dataE[:, 2:]
-        self.e_mass = np.ones(self.numE)/self.numE if not mass else dataE[:,1]
+        self.e_coor = dataE[:,2:] if mass else dataE[:,1:]
+        self.e_mass = dataE[:,1] if mass else np.ones(self.numE)/self.numE
 
     def print_var(self):
         print("h: " + str(self.h))
@@ -55,21 +53,10 @@ class Vot:
     def cluster(self):
         for iterP in range(self.maxIterP):
             self.cost_base = cdist(self.p_coor, self.e_coor, 'sqeuclidean')
-            # t0 = time.clock()
             for iterH in range(self.maxIterH):
                 if self.update_map(iterP,iterH): break
-            # print("map time: " + str(time.clock() - t0) + " seconds")
             if self.update_p_reg(iterP): break
         return False
-
-    def update_p(self, iterP):
-        max_change = 0.0
-        for j in range(self.numP):
-            tmp = np.average(self.e_coor[self.e_idx == j,:], weights=self.e_mass[self.e_idx == j], axis=0)
-            max_change = max(np.amax(self.p_coor[j,:] - tmp),max_change)
-            self.p_coor[j, :] = tmp
-        print("iter " + str(iterP) + ": " + str(max_change))
-        return True if max_change < self.thres else False
 
     def update_map(self, iterP, iterH):
         # update dist matrix
@@ -85,22 +72,55 @@ class Vot:
         # return max
         return True if np.amax(grad) < self.thres else False
 
-    def f_reg_potential(self, x0, x1, alpha=0.5):
-        x0 = x0.reshape(x1.shape)
-        return np.sum(np.sum((x1-x0)**2.0)) + \
-               alpha*np.sum(np.sum((x0[1:,:]-x0[:-1,:])**2.0) + (x0[0,:]-x0[-1,:])**2.0)
+    def update_p(self, iterP):
+        max_change = 0.0
+        for j in range(self.numP):
+            tmp = np.average(self.e_coor[self.e_idx == j,:], weights=self.e_mass[self.e_idx == j], axis=0)
+            max_change = max(np.amax(self.p_coor[j,:] - tmp),max_change)
+            self.p_coor[j, :] = tmp
+        print("iter " + str(iterP) + ": " + str(max_change))
+        return True if max_change < self.thres else False
+
+    def f_potential(self, x, x0, label=None, alpha=0.5):
+        x = x.reshape(x0.shape)
+        return np.sum(np.sum((x0-x)**2.0)) + \
+               alpha*np.sum(np.sum((x[1:,:]-x[:-1,:])**2.0) + (x[0,:]-x[-1,:])**2.0)
+
+    def f_curvature(self, p2, x0, fix, alpha1=0.1, alpha2=0.2):
+        def compute_curvature(x1, y1, z1, x2, y2, z2, x3, y3, z3):
+            a = x1 - 2 * x2 + x3
+            b = y1 - 2 * y2 + y3
+            c = z1 - 2 * z2 + z3
+            dx = x1 - x2
+            dy = y1 - y2
+            dz = z1 - z2
+            numerator = (c * dy - b * dz) ** 2 + (c * dx - a * dz) ** 2 + (b * dx - a * dy) ** 2
+
+            def kp_ds(t):
+                return numerator / np.power((a * t - dx) ** 2 + (b * t - dy) ** 2 + (c * t - dz) ** 2, 2.5)
+
+            sum = (kp_ds(0) + kp_ds(1)) / 2.0
+            # Use matrix operations to replace for loop
+            for t in range(1, 100):
+                t /= 100.0
+                sum += kp_ds(t)
+            sum /= 100.0
+            return sum
+
+        return np.sum(np.sum((x0[1, :] - p2) ** 2.0)) + \
+               alpha2 * np.sum(compute_curvature(p1[0], p1[1], p1[2], p2[0], p2[1], p2[2], p3[0], p3[1], p3[2]))
 
     def update_p_reg(self, iterP):
-        # t0 = time.clock()
         max_change = 0.0
         tmp = np.zeros((self.p_coor.shape))
+        # new controid pos
         for j in range(self.numP):
             tmp[j,:] = np.average(self.e_coor[self.e_idx == j,:], weights=self.e_mass[self.e_idx == j], axis=0)
             max_change = max(np.amax(self.p_coor[j,:] - tmp[j,:]),max_change)
         print("iter " + str(iterP) + ": " + str(max_change))
-        res = minimize(self.f_reg_potential, self.p_coor, method='BFGS', tol=1e-8, args=tmp)
+        # regularize
+        res = minimize(self.f_potential, self.p_coor, method='BFGS', tol=1e-8, args=(tmp,self.p_label))
         self.p_coor = res.x
         self.p_coor = self.p_coor.reshape(tmp.shape)
-        # print("p time: " + str(time.clock() - t0) + " seconds")
-        print(res)
+        # return max change
         return True if max_change < self.thres else False
