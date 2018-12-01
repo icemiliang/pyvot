b6d971803e253752979d785086e41cf4edd11812
Liang Mi <icemiliang@gmail.com>
2018-11-30 19:52:03 -0700

    add comments

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+import time
 
 # def curvature(p1,p2,p3):
 #     def compute_curvature(x1,y1,z1,x2,y2,z2,x3,y3,z3):
@@ -33,7 +34,7 @@ import matplotlib.pyplot as plt
 #         return sum
 #     return compute_curvature(p1[:,0],p1[:,1],p1[:,2],p2[:,0],p2[:,1],p2[:,2],p3[:,0],p3[:,1],p3[:,2])
 
-def curvature(p2, x0, fix, alpha=10.9):
+def curvature(p2, x0, fix, alpha=0.1):
     # p = p.reshape(len(p)//9,3,3)
     # p1 = p[:,0,:]
     # p2 = p[:,1,:]
@@ -51,16 +52,11 @@ def curvature(p2, x0, fix, alpha=10.9):
         dz = z1-z2
         numerator = (c*dy-b*dz)**2+(c*dx-a*dz)**2+(b*dx-a*dy)**2
 
-        def kp_ds (t):
-            return numerator/np.power((a*t-dx)**2+(b*t-dy)**2+(c*t-dz)**2,2.5)
-
-        sum = (kp_ds(0)+ kp_ds(1)) / 2.0
-        # Use matrix operations to replace for loop
-        for t in range(1,100):
-            t/=100.0
-            sum += kp_ds(t)
-        sum /= 100.0
-        return sum
+        t = np.arange(0.0,1.01,0.01)
+        k = numerator/np.power((a*t-dx)**2+(b*t-dy)**2+(c*t-dz)**2,2.5)
+        k[0]/=2
+        k[100] /= 2
+        return np.sum(k)/100
     return np.sum(np.sum((x0[1,:]-p2)**2.0)) + \
            alpha * np.sum(compute_curvature(p1[0],p1[1],p1[2],p2[0],p2[1],p2[2],p3[0],p3[1],p3[2]))
            # alpha*np.sum(compute_curvature(p1[:,0],p1[:,1],p1[:,2],p2[:,0],p2[:,1],p2[:,2],p3[:,0],p3[:,1],p3[:,2]))
@@ -88,7 +84,9 @@ p = np.array([[0.0,0.0,0.0],
               [2.0,0.0,0.0]])
 x0 = p
 fix = p[[0,2],:]
+t0 = time.clock()
 res = minimize(curvature, p[1,:], method='BFGS', tol=1e-8, args=(x0,fix))
+print("time: " + str(time.clock()-t0) + " seconds\n")
 x = res.x
 # x = x.reshape(p.shape)
 print(res)
--- vot.py
+++ vot.py
@@ -1,39 +1,66 @@
-# Variational Wasserstein Clustering
+# Variational Optimal Transportation
 # Author: Liang Mi <icemiliang@gmail.com>
 # Date: Nov 30th 2018
 
-import sys
 import numpy as np
 from scipy.spatial.distance import cdist
 from scipy.optimize import minimize
-# import time
 
 class Vot:
+    """ variational optimal transportation """
+
     def setup(self, max_iter_h=1000, max_iter_p=10, thres=1e-8, rate=0.1):
-        self.h = np.zeros(self.p_num)
+        """ set up parameters
+
+        Args:
+            max_iter_h (int): max number of iterations of clustering
+            max_iter_p (int): max number of iterations of transportation
+            thres (float): threshold to break loops
+            rate  (float): learning rate
+        """
+        
+        self.h = np.zeros(self.np)
         self.thres = thres
         self.learnrate = rate
         self.max_iter_h = max_iter_h
         self.max_iter_p = max_iter_p
 
     def import_data_file(self, pfilename, efilename, mass=False):
+        """ import data from files
+
+        Args:
+            pfilename (string): filename of p
+            efilename (string): filename of e
+            mass (bool): whether data has a mass column
+        """
+
         p_data = np.loadtxt(open(pfilename, "r"), delimiter=",")
         e_data = np.loadtxt(open(efilename, "r"), delimiter=",")
         self.import_data(p_data, e_data, mass)
 
     def import_data(self, p_data, e_data, mass=False):
+        """ import data from files
+
+        Args:
+            p_data np.ndarray(np,dim+): data of p, labels, mass, coordinates, etc
+            e_data np.ndarray(ne,dim+): data of e, labels, mass, coordinates, etc
+            mass (bool): whether data has a mass colum
+        """
+
         self.p_label = p_data[:,0]
-        self.p_num = self.p_label.size
+        self.np = self.p_label.size
         self.p_coor = p_data[:,2:] if mass else p_data[:,1:]
-        self.p_dirac = p_data[:,1] if mass else np.ones(self.p_num)/self.p_num
-        self.p_mass = np.zeros(self.p_num)
+        self.p_dirac = p_data[:,1] if mass else np.ones(self.np)/self.np
+        self.p_mass = np.zeros(self.np)
 
         self.e_label = e_data[:,0]
-        self.e_num = self.e_label.size
+        self.ne = self.e_label.size
         self.e_coor = e_data[:,2:] if mass else e_data[:,1:]
-        self.e_mass = e_data[:,1] if mass else np.ones(self.e_num)/self.e_num
+        self.e_mass = e_data[:,1] if mass else np.ones(self.ne)/self.ne
 
     def print_var(self):
+        """ print attributes to console """
+
         print("h: " + str(self.h))
         print("cost matrix: \n" + str(self.cost))
         print("p label: " + str(self.p_label))
@@ -47,20 +74,40 @@ class Vot:
         print("e coor: \n" + str(self.e_coor))
 
     def cluster(self, reg=0):
+        """ compute Wasserstein clustering
+
+        Args:
+            reg int: flag for regularization, 0 means no regularization
+
+        See Also
+        --------
+        update_p : update p
+        update_map: computer optimal transportation
+        """
+
         for iter_p in range(self.max_iter_p):
             self.cost_base = cdist(self.p_coor, self.e_coor, 'sqeuclidean')
             for iter_h in range(self.max_iter_h):
                 if self.update_map(iter_p,iter_h): break
             if self.update_p(iter_p, reg): break
-        return False
 
     def update_map(self, iter_p, iter_h):
+        """ update each p to the centroids of its cluster
+
+        Args:
+            iter_p int: iteration index of clustering
+            iter_h int: iteration index of transportation
+
+        Returns:
+            float: max change of derivative, small max means convergence
+        """
+
         # update dist matrix
         self.cost = self.cost_base - self.h[:, np.newaxis]
         # find nearest p for each e and add mass to p
         self.e_idx = np.argmin(self.cost, axis=0)
         self.e_predict = self.p_label[self.e_idx]
-        for j in range(self.p_num):
+        for j in range(self.np):
             self.p_mass[j] = np.sum(self.e_mass[self.e_idx == j])
         # update gradient and h
         grad = self.p_mass - self.p_dirac
@@ -69,48 +116,56 @@ class Vot:
         return True if np.amax(grad) < self.thres else False
 
     def update_p(self, iter_p, reg=0):
-        if reg == 0:
-            return self.update_p_noreg(iter_p)
-        elif reg == 1:
+        if reg == 1:
             return self.update_p_reg_potential(iter_p)
         elif reg == 2:
             return self.update_p_reg_curvature(iter_p)
         else:
-            sys.exit('[ ERROR ] Unknown regularization type\n')
+            return self.update_p_noreg(iter_p)
 
     def update_p_noreg(self, iter_p):
+        """ update each p to the centroids of its cluster
+
+        Args:
+            iter_p int: iteration index
+
+        Returns:
+            float: max change of p, small max means convergence
+        """
+
         max_change = 0.0
         # update p to the centroid of its clustered e samples
-        for j in range(self.p_num):
+        for j in range(self.np):
             tmp = np.average(self.e_coor[self.e_idx == j,:], weights=self.e_mass[self.e_idx == j], axis=0)
             # check if converge
-            max_change = max(np.amax(self.p_coor[j,:] - tmp),max_change)
+            max_change = np.amax(np.amax(self.p_coor[j,:] - tmp), max_change)
             self.p_coor[j,:] = tmp
         print("iter " + str(iter_p) + ": " + str(max_change))
         # return max p coor change
         return True if max_change < self.thres else False
 
     def update_p_reg_potential(self, iter_p):
-        def f(self, x, x0, label=None, alpha=0.1):
-            """ Objective function incorporating p labels
+        def f(p, p0, label=None, alpha=0.1):
+            """ objective function incorporating labels
 
             Args:
-                x (np.array[p_num,dim]):   p
-                x0 (np.array[p_num,dim]):  centroids of e
-                label (np.array[p_num,1]): labels of p
-                alpha (float): regularizer weight
+                p  np.array(np,dim):   p
+                p0 np.array(np,dim):  centroids of e
+                label np.array(np,): labels of p
+                alpha float: regularizer weight
 
             Returns:
-                float: total energy sum(|x-x0|^2) + sum(|xi-xj|^2)
+                float: f = sum(|x-x0|^2) + alpha*sum(|xi-xj|^2)
             """
-            x = x.reshape(x0.shape)
-            return np.sum(np.sum((x0 - x) ** 2.0)) + \
-                   alpha * np.sum(np.sum((x[1:, :] - x[:-1, :]) ** 2.0) + (x[0, :] - x[-1, :]) ** 2.0)
+
+            p = p.reshape(p0.shape)
+            return np.sum(np.sum((p0 - p)**2.0)) + \
+                   alpha * np.sum(np.sum((p[1:,:] - p[:-1,:])**2.0) + (p[0,:] - p[-1,:])**2.0)
 
         max_change = 0.0
         tmp = np.zeros((self.p_coor.shape))
         # new controid pos
-        for j in range(self.p_num):
+        for j in range(self.np):
             tmp[j,:] = np.average(self.e_coor[self.e_idx == j,:], weights=self.e_mass[self.e_idx == j], axis=0)
             max_change = max(np.amax(self.p_coor[j,:] - tmp[j,:]),max_change)
         print("iter " + str(iter_p) + ": " + str(max_change))
@@ -122,29 +177,49 @@ class Vot:
         return True if max_change < self.thres else False
 
     def update_p_reg_curvature(self, iter_p):
+        def f(p, p0, pfix, alpha1=0.1, alpha2=0.1):
+            """ objective function incorporating length and curvature
 
-        def f(self, x, x0, xfix, alpha1=0.1, alpha2=0.2):
-            def compute_curvature(x1, y1, z1, x2, y2, z2, x3, y3, z3):
+            Args:
+                p  np.ndarray(np-pfix_num,dim): p
+                p0 np.ndarray(np-pfix_num,dim): centroids of e
+                pfix np.ndarray(fix_num,): labels of p
+                alpha1 float: weight of regularizer length
+                alpha2 float: weight of regularizer curvature
+
+            Returns:
+                float: f = sum(|x-x0|^2) + alpha1*sum(length(xj,xj-1) + alpha2*sum(curvature(xj,xj-1,xj-2)))
+            """
+
+            def length(x1,y1,z1,x2,y2,z2,x3,y3,z3):
                 a = x1 - 2 * x2 + x3
                 b = y1 - 2 * y2 + y3
                 c = z1 - 2 * z2 + z3
                 dx = x1 - x2
                 dy = y1 - y2
                 dz = z1 - z2
-                e_numrator = (c*dy - b*dz)**2 + (c*dx - a*dz)**2 + (b*dx - a*dy)**2
 
-                def kp_ds(t):
-                    return e_numrator / np.power((a*t - dx)**2 + (b*t - dy)**2 + (c*t - dz)**2, 2.5)
+                t = np.array(0.0,1.01,0.01)
+                ds = np.sqrt((a*t - dx)**2 + (b*t - dy)**2 + (c*t - dz)**2)
+                return np.sum(ds)
 
-                sum = (kp_ds(0) + kp_ds(1)) / 2.0
-                # Use matrix operations to replace for loop
-                for t in range(1, 100):
-                    t /= 100.0
-                    sum += kp_ds(t)
-                sum /= 100.0
-                return sum
+            def curvature(x1, y1, z1, x2, y2, z2, x3, y3, z3):
+                a = x1 - 2 * x2 + x3
+                b = y1 - 2 * y2 + y3
+                c = z1 - 2 * z2 + z3
+                dx = x1 - x2
+                dy = y1 - y2
+                dz = z1 - z2
+                numerator = (c*dy - b*dz)**2 + (c*dx - a*dz)**2 + (b*dx - a*dy)**2
 
-            return np.sum(np.sum((x0[1, :] - p2) ** 2.0)) + \
-                   alpha2 * np.sum(compute_curvature(p1[0], p1[1], p1[2], p2[0], p2[1], p2[2], p3[0], p3[1], p3[2]))
+                t = np.arange(0.0, 1.01, 0.01)
+                k = numerator / np.power((a*t - dx)**2 + (b*t - dy)**2 + (c*t - dz)**2, 2.5)
+                k[0] /= 2
+                k[100] /= 2
+                return np.sum(k)/100
 
-
+            cost = 0
+            cost +=  np.sum(np.sum((p0 - p)**2.0)) + \
+                     alpha1 * np.sum(length())+\
+                     alpha2 * np.sum(curvature())
+            return cost
