21d9f6483a779e8ad00b3ccc9562acd1e08dea21
Liang Mi <icemiliang@gmail.com>
2019-01-07 12:07:12 -0700

    update pics

--- demo_transform.py
+++ demo_transform.py
@@ -94,7 +94,7 @@ ot_reg.setup(max_iter_p = 10, max_iter_h = 2000)
 # ----- plot before ----- #
 cp = [vot.color_blue, vot.color_red]
 cp = [cp[label] for index,label in np.ndenumerate(ot_reg.p_label)]
-plt.subplot(234); plt.xlim(xmin,1.5); plt.ylim(ymin,ymax); plt.grid(True); plt.title('w reg before')
+plt.subplot(234); plt.xlim(xmin,1.5); plt.ylim(ymin,ymax); plt.grid(True); plt.title('w/ reg before')
 plt.scatter(ot_reg.e_coor[:,0], ot_reg.e_coor[:,1], marker='.', color=vot.color_light_grey, zorder=2)
 plt.scatter(p_coor_before[:,0], p_coor_before[:,1], marker='o', color=cp, zorder=3)
 
@@ -112,7 +112,7 @@ ot_reg.cluster()
 p_coor_after = np.copy(ot_reg.p_coor)
 map = [[tuple(p1),tuple(p2)] for p1,p2 in zip(p_coor_before.tolist(), p_coor_after.tolist())]
 lines = mc.LineCollection(map, colors=vot.color_light_grey)
-fig235 = plt.subplot(235); plt.xlim(xmin,xmax); plt.ylim(ymin,ymax); plt.grid(True); plt.title('w reg map')
+fig235 = plt.subplot(235); plt.xlim(xmin,xmax); plt.ylim(ymin,ymax); plt.grid(True); plt.title('w/ reg map')
 fig235.add_collection(lines)
 plt.scatter(p_coor_before[:,0], p_coor_before[:,1], marker='o', color=cp,zorder=3)
 plt.scatter(p_coor_after[:,0], p_coor_after[:,1], marker='o', facecolors='none', linewidth=2, color=cp, zorder=2)
@@ -123,7 +123,7 @@ ce = [vot.color_light_blue, vot.color_light_red]
 ce = [ce[label] for index,label in np.ndenumerate(le)]
 cp = [vot.color_dark_blue, vot.color_red]
 cp = [cp[label] for index,label in np.ndenumerate(ot_reg.p_label)]
-plt.subplot(236); plt.xlim(xmin,xmax); plt.ylim(ymin,ymax); plt.grid(True); plt.title('w reg after')
+plt.subplot(236); plt.xlim(xmin,xmax); plt.ylim(ymin,ymax); plt.grid(True); plt.title('w/ reg after')
 plt.scatter(ot_reg.e_coor[:,0], ot_reg.e_coor[:,1], marker='.', color=ce, zorder=2)
 plt.scatter(p_coor_after[:,0], p_coor_after[:,1], marker='o', facecolors='none', linewidth=2, color=cp, zorder=3)
 
