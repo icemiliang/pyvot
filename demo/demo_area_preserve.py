8a3da6598d7f112faeae41bddc316501d3b90e39
icemiliang <icemiliang@gmail.com>
2019-04-30 23:16:07 -0700

    refactoring

--- demo/demo_area_preserve.py
+++ demo/demo_area_preserve.py
@@ -23,12 +23,11 @@ import utils as vot
 import time
 
 # ----- set up ot ------ #
-ot = VotAreaPreserve()
-ot.import_data_from_file('data/p.csv')
-ot.setup(max_iter=5000, ratio=200, rate=0.2, dim = 2)
+ot = VotAreaPreserve(max_iter=5000, ratio=200, rate=0.2, dim=2)
+ot.import_data_from_file('data/p.csv', has_label=True)
 
 # ----- plot before ----- #
-p_coor_before = np.copy(ot.p_coor)
+p_coor_before = np.copy(ot.Xp)
 plt.figure(figsize=(12,4))
 
 cp = [vot.color_blue, vot.color_red]
@@ -43,7 +42,7 @@ tock = time.clock()
 print(tock-tick)
 
 # ------ plot map ------- #
-p_coor_after = np.copy(ot.p_coor)
+p_coor_after = np.copy(ot.Xp)
 map = [[tuple(p1),tuple(p2)] for p1,p2 in zip(p_coor_before.tolist(), p_coor_after.tolist())]
 lines = mc.LineCollection(map, colors=vot.color_light_grey)
 fig232 = plt.subplot(132); plt.xlim(-1,1); plt.ylim(-1,1); plt.grid(True); plt.title('area preserving map')
@@ -58,7 +57,7 @@ ce = [ce[label] for index,label in np.ndenumerate(le)]
 cp = [vot.color_dark_blue, vot.color_red]
 cp = [cp[label] for index,label in np.ndenumerate(ot.p_label)]
 plt.subplot(133); plt.xlim(-1,1); plt.ylim(-1,1); plt.grid(True); plt.title('after')
-plt.scatter(ot.e_coor[:,0], ot.e_coor[:,1], marker='.', color=ce, zorder=2, s=0.5)
+plt.scatter(ot.X_e[:, 0], ot.X_e[:, 1], marker='.', color=ce, zorder=2, s=0.5)
 plt.scatter(p_coor_after[:,0], p_coor_after[:,1], marker='o', facecolors='none', linewidth=2, color=cp, zorder=3)
 
 # ---- plot and save ---- #
